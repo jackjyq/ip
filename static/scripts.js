9c9146bc5ee70c3790f32ccc45f0e76567ee831e
Jack Jiang <jackjyq@outlook.com>
2022-11-05 10:01:57 +0800

    add more info about the screen

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,24 @@
-// Get screen resolution
+// reduce fraction
+// https://stackoverflow.com/a/4652513
+// Reduce a fraction by finding the Greatest Common Divisor and dividing by it.
+function reduceFraction(numerator, denominator) {
+  if (isNaN(numerator) || isNaN(denominator)) return NaN;
+  var gcd = function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+  };
+  gcd = gcd(numerator, denominator);
+  return [numerator / gcd, denominator / gcd];
+}
+
+// Get the screen detail
 // https://stackoverflow.com/questions/2242086/how-to-detect-the-screen-resolution-with-javascript
 document.getElementById("resolution").innerHTML =
   window.screen.width + " x " + window.screen.height;
-// document.getElementById("devicePixelRatio").innerHTML = window.devicePixelRatio;
+document.getElementById("devicePixelRatio").innerHTML = window.devicePixelRatio;
+
+reducedPair = reduceFraction(window.screen.width, window.screen.height);
+document.getElementById("aspectRatio").innerHTML =
+  reducedPair[0] + " : " + reducedPair[1];
 
 // Get device location
 const latitude_element = document.getElementById("latitude");
